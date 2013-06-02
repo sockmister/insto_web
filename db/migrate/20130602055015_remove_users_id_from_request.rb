c0f0dbd52ded3d18f15c8712fcd14627443e61c9
pohchiat@gmail.com <pohchiat@gmail.com>
2013-06-02 21:31:43 +0800

    Switched to postgresql.

--- db/migrate/20130602055015_remove_users_id_from_request.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUsersIdFromRequest < ActiveRecord::Migration
-  def up
-  	remove_column :requests, :users_id
-  end
-
-  def down
-  	add_column :requests, :users_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130602055015) do
+ActiveRecord::Schema.define(:version => 20130602054658) do
 
   create_table "locations", :force => true do |t|
     t.string   "faculty"
