7ee9a7c11317bfbde277c40e718e7ad9e630c617
pohchiat@gmail.com <pohchiat@gmail.com>
2013-06-09 15:57:22 +0800

    Changed submission save url.

--- insto-web/app/models/submission.rb
+++ insto-web/app/models/submission.rb
@@ -3,7 +3,9 @@ class Submission < ActiveRecord::Base
 
   has_attached_file :image, 
   	:styles => { :thumb => "100x100", }, 
-  	:convert_options => { :thumb => "-quality 75 -strip" }
+  	:convert_options => { :thumb => "-quality 75 -strip" },
+  	:url => "/public/images/:id/:style_:basename.:extension",
+   	:path => ":rails_root:url"
 
   def image_url
   	image.url(:original)
